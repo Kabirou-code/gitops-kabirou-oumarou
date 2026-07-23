449ad01e421f2869f7bfd70908a6cfe2ca63a38f
Kabirou Oumarou <kabirou.oumarou@exemple.com>
2026-07-23 16:33:08 +0000

    Initial commit: app Node.js + chart Helm personnalisés

---
+++ app/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send(`
+    <html>
+      <head><title>Kabirou Oumarou - GitOps K3s</title></head>
+      <body style="font-family: Arial; text-align: center; margin-top: 100px;">
+        <h1>Kabirou Oumarou</h1>
+        <p>M1 SSI - ESMT Dakar - Projet GitOps observable sur Kubernetes</p>
+        <p>Version: 1.0.0</p>
+      </body>
+    </html>
+  `);
+});
+
+app.listen(PORT, '0.0.0.0', () => console.log(`App running on port ${PORT}`));
